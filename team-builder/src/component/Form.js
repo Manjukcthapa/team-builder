80127ddfe145d866d6cbe1a6bcd6ea90d571d20b
Manju KC <kcmanjuthapa@gmail.com>
2019-09-18 15:32:18 -0400

    created form inside Form component

--- team-builder/src/component/Form.js
+++ team-builder/src/component/Form.js
@@ -1,1 +1,14 @@
 import React, { useState } from "react";
+
+const Form = props => {
+    const [member, setMember] = useState({});
+  return (
+    <form>
+      <input id="name" type="text" name="name" />
+      <input id="email" type="email" name="email" />
+      <input id="role" type="text" name="role" />
+    </form>
+  );
+};
+
+export default Form;
