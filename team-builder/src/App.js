82a3345c9f8e0ad756a583b030fe73a7b3aced7c
Manju KC <kcmanjuthapa@gmail.com>
2019-09-18 15:09:31 -0400

    added state in App.js

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,7 +1,12 @@
 import React, { useState } from "react";
 import './App.css';
 function App() {
-
+const [member, setMember] = useState([
+  {id: 0, name: "Tyler Thompson", email: "Tyler34@gmail.com", role: "TL"},
+    {id: 1, name: "sabrina", email: "sabrina@scottbeeker", role: "student"},
+    {id: 2, name: "james bond", email: "itsme@bond", role: "student"},
+    {id: 3, name: "greenland jones", email: "itsme@mikejones", role: "student"},
+]);
 
   return(
     <div>
