6bd42ace58fb5d7823ec4cbcbac04d5056da9473
Manju KC <kcmanjuthapa@gmail.com>
2019-09-18 14:57:52 -0400

    imported the useState in App.js

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,15 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
+function App() {
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      <h1>hello</h1>
-      </div>
-    );
-  }
+
+  return(
+    <div>
+
+    </div>
+  )
 }
 
 export default App;
