c058dc484843d11de8ecf25c11e33a19a1562919
Manju KC <kcmanjuthapa@gmail.com>
2019-09-18 15:37:01 -0400

    added handlechange and value inside the input

--- team-builder/src/component/Form.js
+++ team-builder/src/component/Form.js
@@ -1,12 +1,30 @@
 import React, { useState } from "react";
 
 const Form = props => {
-    const [member, setMember] = useState({});
+  const [member, setMember] = useState({name: "", email: "", role:""});
   return (
     <form>
-      <input id="name" type="text" name="name" />
-      <input id="email" type="email" name="email" />
-      <input id="role" type="text" name="role" />
+      <input
+        id="name"
+        type="text"
+        name="name"
+        onChange={handleChanges}
+        value={member.name}
+      />
+      <input
+        id="email"
+        type="email"
+        name="email"
+        onChange={handleChanges}
+        value={member.email}
+      />
+      <input
+        id="role"
+        type="text"
+        name="role"
+        onChange={handleChanges}
+        value={member.role}
+      />
     </form>
   );
 };
